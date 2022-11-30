49e57e12d990ca326416707f0f19919c82b04186
AniAvazneli <avazneli.ani@gmail.com>
2022-11-30 20:02:45 +0400

    feat: userRouter

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import bodyParser from "body-parser";
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 import connectMongo from "./cofig/mongo.js";
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 dotenv.config();
 connectMongo();
 
 app.use(bodyParser.json());
+
+app.use("/api", userRouter);
 
 app.listen(3000);
