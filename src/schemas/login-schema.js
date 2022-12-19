004256731331c7438d99f54bc97ae173a5b61da0
hilu <nikanozadze1010@gmail.com>
2022-12-19 16:33:45 +0400

    feat: create login schema

---
+++ src/schemas/login-schema.js
@@ -0,0 +1,32 @@
+import Joi from "joi";
+import User from "../models/User";
+
+const determineIfUserExists = (user) => (value, helpers) => {
+  if (!user) {
+    return helpers.message({
+      en: "user with this email does not exists",
+      geo: "მომხმარებელი აღნიშნული მეილით არ მოიძებნა",
+    });
+  }
+  return value;
+};
+
+const loginSchema = async (data) => {
+  const user = await User.findOne({ email: data.email });
+
+  return Joi.object({
+    email: Joi.string()
+      .custom(determineIfUserExists(user))
+      .required()
+      .messages({
+        "string.base": "email should be a string",
+        "any.required": "email is required",
+      }),
+    password: Joi.string().required().messages({
+      "string.base": "password should be a string",
+      "any.required": "password is required",
+    }),
+  });
+};
+
+export default loginSchema;
