d6e335d5b4833bd45b61840678f5f38e41449b28
hilu <nikanozadze1010@gmail.com>
2023-01-09 18:40:06 +0400

    feat: add dotenv in auth

--- src/auth/facebook.js
+++ src/auth/facebook.js
@@ -1,26 +1,29 @@
-import passport from 'passport';
-import FacebookStrategy from 'passport-facebook';
+import passport from "passport";
+import FacebookStrategy from "passport-facebook";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_ID,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-  ));
-  
-  export const Facebook = passport.authenticate('facebook');
-  
-  export const FacebookCallback = 
-    // passport.authenticate('facebook', { failureRedirect: '/login' }),
-    function(req, res) {
-      // Successful authentication, redirect home.
-      res.redirect('/');
-    };
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+export const Facebook = passport.authenticate("facebook");
+
+export const FacebookCallback =
+  // passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  };
