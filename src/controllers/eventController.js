9a0689c85ed0d46cc22bc5004b3a1dc67eef9981
AniAvazneli <avazneli.ani@gmail.com>
2023-01-20 22:05:34 +0400

    created update adn delete  functions for events

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -28,3 +28,51 @@ export const createEvent = async (req, res) => {
 
   return res.status(201).json({ message: "Event created successfully" });
 };
+
+export const updateEvent = async (req, res) => {
+  const { params, body } = req;
+
+  const event = await Event.findOne({ id: params.id });
+
+  if (!event) {
+    return res
+      .status(422)
+      .json({ message: "there is no event with this id" });
+  }
+
+  const validator = await createEventSchema(body);
+  const { value, error } = validator.validate(body);
+
+  if (error) {
+    return res.status(422).json(error.details);
+  }
+
+  const {
+    name
+  } = value;
+
+  await Event.findOneAndUpdate(
+    { id: params.id },
+    {
+      name
+    }
+  );
+
+  return res.status(200).json({ message: "event update successfully" });
+};
+
+export const deleteEvent = async (req, res) => {
+  const { params } = req;
+
+  const event = await Event.findOne({ id: params.id });
+
+  if (!event) {
+    return res
+      .status(422)
+      .json({ message: "there is no event with this id" });
+  }
+
+  await Event.findOneAndDelete({ id: params.id });
+
+  return res.status(200).json({ message: "event removed successfully" });
+};
---
+++ src/routes/eventRouter.js
@@ -0,0 +1,2 @@
+import express from "express";
+import {getAllEvents, createEvent, updateEvent, deleteEvent} from '../controllers/eventController.js'
